3ae0b322fa5834aead2f42451d062b723a22becd
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-12-20 14:22:52 -0800

    Changed setupNewMachine to start.bat

---
+++ installChoco.ps1
@@ -0,0 +1,15 @@
+$testchoco = powershell choco -v
+# obtained from this thread on spiceworks: https://community.spiceworks.com/topic/2203658-check-if-choco-already-installed-and-install-if-not
+if(-not($testchoco)){
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Output "Chocolatey Version $testchoco is already installed"
+}
+
+# or
+
+if(test-path "C:\ProgramData\chocolatey\choco.exe"){
+
+}
